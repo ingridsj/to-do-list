9bca167f910e642eccce32368d14f8df7d60e3eb
ingridsj <ingridsjalmeida@gmail.com>
2021-05-20 18:50:35 -0300

    add: 'lis' removal function

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const formAddTodo = document.querySelector('.form-add')
+const deleteTodo = document.querySelector('.button-delete')
+
 const todosContainer = document.querySelector('.ul-container')
 
 formAddTodo.addEventListener('submit', (event) => {
   event.preventDefault()
 
   const inputValue = event.target.add.value.trim()
-  if(inputValue.length) {
+  if (inputValue.length) {
     todosContainer.innerHTML += `
     <li class="li-container">
       <span>${inputValue}</span>
-      <button class="button-delete"><img src="/assets/lixeira.svg" alt="Deletar"></button>
+      <img class="button-delete" src="/assets/lixeira.svg" alt="Deletar">
     </li>
     `
     event.target.reset()
+  }
+})
+
+todosContainer.addEventListener('click', () => {
+  const clickedElement = event.target
+
+  if (Array.from(clickedElement.classList).includes('button-delete')) {
+    clickedElement.parentElement.remove()
   }
 })
